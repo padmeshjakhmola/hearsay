31996a883a18e82a0210736048cf1a4f797b82f5
Padmesh Jakhmola <prankshadow@gmail.com>
2025-04-24 21:09:07 +0530

    feaet:added aws s3 upload

--- src/utils/aws.ts
+++ src/utils/aws.ts
@@ -6,7 +6,7 @@ const s3 = new S3Client({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
-  region: process.env.AWS_BUCKET_NAME!,
+  region: process.env.AWS_REGION!,
 });
 
 export default s3;
--- src/utils/downloadMedia.ts
+++ src/utils/downloadMedia.ts
@@ -1,9 +1,11 @@
+import { Upload } from "@aws-sdk/lib-storage";
 import axios from "axios";
 import fs from "node:fs";
 import { mkdirSync } from "node:fs";
 
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import s3 from "./aws.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,24 +20,37 @@ export async function downloadMedia(opts: {
 
   try {
     const ext = (opts.mime_type.split("/")[1] || "bin").replace("+", "_");
-    const dirPath = path.join(__dirname, "tmp");
-    const filePath = path.join(dirPath, `${opts.id}.${ext}`);
+    // const dirPath = path.join(__dirname, "tmp");
+    // const filePath = path.join(dirPath, `${opts.id}.${ext}`);
+    // mkdirSync(dirPath, { recursive: true });
 
-    mkdirSync(dirPath, { recursive: true });
+    // console.log("yahayaha:", filePath);
+    // mkdirSync(dirPath, { recursive: true });
 
+    // await new Promise<void>((resolve, reject) => {
+    //   res.data
+    //     .pipe(fs.createWriteStream(filePath))
+    //     .on("finish", resolve)
+    //     .on("error", reject);
+    // });
+
+    const key = `${opts.id}.${ext}`;
     const res = await axios.get(opts.url, {
       responseType: "stream",
       headers: { Authorization: `Bearer ${opts.token}` },
     });
 
-    await new Promise<void>((resolve, reject) => {
-      res.data
-        .pipe(fs.createWriteStream(filePath))
-        .on("finish", resolve)
-        .on("error", reject);
-    });
+    await new Upload({
+      client: s3,
+      params: {
+        Bucket: process.env.AWS_BUCKET_NAME!,
+        Key: key,
+        Body: res.data,
+        ContentType: opts.mime_type,
+      },
+    }).done();
 
-    return filePath;
+    return key;
   } catch (error) {
     console.error("error_saving_file", error);
     throw error;
